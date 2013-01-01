516be8a0bc852f2ea5756710487e9db6e57d25d8
ToshinoriMaeno <tmaeno-git@m.qmail.jp>
2013-01-01 17:47:27 +0900

    modified:   postdns.net.lua

--- postdns.net.lua
+++ postdns.net.lua
@@ -12,9 +12,6 @@ cname("www", _a, ttl)
 
 a(concat("mail", _a),	ipaddr, ttl)
 a(concat("mailhost", _a),	ipaddr, ttl)
-a(concat("lists", _a),		ipaddr, ttl)
 
 mx(_a, concat("mailhost", _a), 10, ttl)
-mx(concat("lists", _a), concat("mailhost", _a), 10, ttl)
 txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
-txt(concat("lists", _a), "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
