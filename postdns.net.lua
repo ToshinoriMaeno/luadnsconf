5b40ede55b18d73597ee2c6a09d9c674d1a5a30c
ToshinoriMaeno <tmaeno-git@m.qmail.jp>
2013-01-02 17:50:37 +0900

    modified:   postdns.net.lua

--- postdns.net.lua
+++ postdns.net.lua
@@ -1,17 +1,17 @@
--- File: hayamiz.com.lua
+-- File: postdns.net.lua
 -- Variable _a is replaced with zone name by Luadns
 -- _a = "postdns.net"
 
 -- parameters
-ttl = 3600
+-- ttl = 3600
 ipaddr = "49.212.139.194"
 
 -- configurations
-a(_a, ipaddr, ttl)
-cname("www", _a, ttl)
+a(_a, ipaddr)
+a(concat("www", _a),ipaddr)
+a(concat("mx", _a),ipaddr)
+a(concat("ns.qmail", _a),ipaddr)
+ns("qmail", concat("ns.qmail",_a))
 
-a(concat("mail", _a),	ipaddr, ttl)
-a(concat("mailhost", _a),	ipaddr, ttl)
-
-mx(_a, concat("mailhost", _a), 10, ttl)
-txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mailhost." .. _a .. " mx:mailhost." .. _a .. " mx:" .. _a .. " -all", ttl)
+mx(_a, concat("mx", _a), 0)
+txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mx." .. _a .. " mx:mx." .. _a .. " mx:" .. _a .. " -all")
