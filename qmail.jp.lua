5d88b85b350c0c2d255bd71dbde5b371022355a6
ToshinoriMaeno <tmaeno-git@m.qmail.jp>
2013-01-02 21:02:44 +0900

    modified:   qmail.jp.lua 	modified:   www.qmail.jp.lua

--- qmail.jp.lua
+++ qmail.jp.lua
@@ -4,14 +4,14 @@
 
 -- parameters
 -- ttl = 3600
-ipaddr = "49.212.139.194"
+ipaddr = "14.192.44.5"
 
 -- configurations
 a(_a, ipaddr)
 a(concat("www", _a),ipaddr)
 a(concat("mx", _a),ipaddr)
-a(concat("ns.qmail", _a),ipaddr)
-ns("qmail", concat("ns.qmail",_a))
+a(concat("ns", _a),ipaddr)
+ns("zzz", concat("ns",_a))
 
 mx(_a, concat("mx", _a), 0)
 txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mx." .. _a .. " mx:mx." .. _a .. " mx:" .. _a .. " -all")
--- www.qmail.jp.lua
+++ www.qmail.jp.lua
@@ -8,10 +8,9 @@ ipaddr = "49.212.139.194"
 
 -- configurations
 a(_a, ipaddr)
-a(concat("www", _a),ipaddr)
 a(concat("mx", _a),ipaddr)
-a(concat("ns.qmail", _a),ipaddr)
-ns("qmail", concat("ns.qmail",_a))
+a(concat("ns", _a),ipaddr)
+ns("qmail", concat("ns",_a))
 
 mx(_a, concat("mx", _a), 0)
 txt(_a, "v=spf1 a mx ptr ip4:" .. ipaddr .. " ptr:mx." .. _a .. " mx:mx." .. _a .. " mx:" .. _a .. " -all")
